435b0647fbaddaee2124065a1299e8e3aa8c2bc2
VirtualMagpie <jeremy.denechaud@free.fr>
2024-11-08 18:46:41 +0100

    Init project

---
+++ src/main/java/fr/virtualmagpie/GamePanel.java
@@ -0,0 +1,15 @@
+package fr.virtualmagpie;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+
+    public static final int WIDTH = 1100;
+    public static final int HEIGHT = 800;
+
+    public GamePanel() {
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setBackground(Color.BLACK);
+    }
+}
